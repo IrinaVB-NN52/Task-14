4f497e8574366cf4b67ed2b4b02c012d9bbd2c6c
Ирина Баранова <irina.baranova14@outlook.com>
2024-01-20 10:38:08 +0300

    Добавьте файлы проекта.

---
+++ Task-14/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_14
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Product> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products =new ObservableCollection<Product>();
+            products.Add(new Product()
+            {
+                ProdName = "Яблоко",
+                ProdPrice = 120,
+                ProdImage ="Data/яблолко.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Суп",
+                ProdPrice = 500,
+                ProdImage ="Data/Суп.jpg",
+                ProductCategory = ProductCategorys.Food
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Холодильник",
+                ProdPrice = 80000,
+                ProdImage ="Data/холодильник.jpg",
+                ProductCategory = ProductCategorys.Applinces 
+            });
+            products.Add(new Product()
+            {
+                ProdName = "Стиральная машина",
+                ProdPrice = 50000,
+                ProdImage ="Data/Стир машинка.jpg",
+                ProductCategory = ProductCategorys.Applinces
+            });
+            listBox.ItemsSource = products;
+        }
+    }
+    
+}
---
+++ Task-14/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+
+{
+    public enum ProductCategorys
+
+    {
+        Food,
+        Applinces
+    }
+    public  class Product
+    {
+       public string ProdName {get; set;} 
+        public decimal ProdPrice { get; set; }
+        public string  ProdImage { get; set; }
+        public ProductCategorys  ProductCategory  { get; set; }
+    }
+}
